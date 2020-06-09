5f49ff78537f70c04ec9323abadccf0542e6d9b7
Rene Montoya <rmontoya.we@gmail.com>
2020-06-09 14:49:13 -0700

    finishing lab6 one part

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
 // ================= Global ========================
 var openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+
 // ================= Random Number Generator ============
+
 function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random()*((max + 1) - min) + min);
 }
-
-function hourlyService(seattle) {
-  for (var i = 0; i < openHours; i++){
-    var footTraffic = randomNumber(seattle.min, seattle.max);
-    var hourlyTotalSales = Math.round(footTraffic*seattle.avg);
-    seattle.hourlySales.push(hourlyTotalSales);
-    seattle.dailySales += hourlyTotalSales;
-  }
-  console.log(footTraffic);
-}
-
-// ================= Seattle ===============
+// ================ Store Details ================
 
 var seattle = {
   location : 'Seattle',
-  min : 23,
-  max : 64,
-  avg : 64,
+  minNumCustomers : 23,
+  maxNumCustomers : 65,
+  avgNumCookies : 6.3,
   hourlySales : [], // this will be assigned the sales numbers calculated
   dailySales : 0, // will be the sum of hourly sales in one day
-
+  calculateCustomersOnce : function(){
+    // this function generates a random number between the min and max values in the object
+    var customers = randomNumber(this.minNumCustomers,this.maxNumCustomers);
+    return customers;
+  },
+  calculateCookiesAllHours : function(){
+    // this function will calculate the cookies for the whole day
+    for (var i = 0; i < openHours.length; i++) {
+      var cookies = Math.ceil(this.calculateCustomersOnce()*this.avgNumCookies);
+      console.log(cookies);
+      this.hourlySales.push(cookies); //this stores cookies into end of array
+    }
+  },
   // This function will render to the page the store list
-  renderToPage : function () {
+  renderToPage : function(){
     // 1. target
     var storeList = document.getElementById('seattle-ul');
     // 2. new element
@@ -36,19 +41,17 @@ var seattle = {
     // 3. put it on the page
     storeList.appendChild(storeStats);
 
-    for (var i = 0; i < openHours; i++){
+    for (var i = 0; i < openHours.length; i++){
       storeStats = document.createElement('li');
-      storeStats.textContent = openHours[i] + ' : ' + this.hourlySales + ' cookies';
+      storeStats.textContent = openHours[i] + ' ' + this.hourlySales[i] + ' cookies';
       console.log(this.calculateEachHourCookieSales);
       storeList.appendChild(storeStats);
     }
-
-    storeStats = document.createElement('li');
-    storeStats.textContent = 'Total: ' + this.dailySales + ' cookies';
-    storeList.appendChild(storeStats);
   }
 };
 
+
 console.log('hourlySales', seattle.hourlySales);
-hourlyService(seattle);
+seattle.calculateCustomersOnce();
+seattle.calculateCookiesAllHours();
 seattle.renderToPage();
