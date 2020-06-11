a436b1552c4f4985dcd4b7052600fd1d2d0f2214
Rene Montoya <rmontoya.we@gmail.com>
2020-06-11 02:59:09 -0700

    making stuff dynamic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // ================= Global ========================
 var openHours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
-var locationUls = ['seattle-ul', 'tokyo-ul', 'dubai-ul', 'paris-ul', 'lima-ul'];
+// var locationUls = ['seattle-ul', 'tokyo-ul', 'dubai-ul', 'paris-ul', 'lima-ul'];
 
 // ================= Random Number Generator ============
 
